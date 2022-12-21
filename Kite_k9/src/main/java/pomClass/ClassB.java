78a59598b8eecf68309f4ab46a247d6693495e67
Krushna K9 <kudwant09@gmail.com>
2022-12-21 11:29:46 +0530

    Adding ClassA, ClassB and ClassC by k9

---
+++ Kite_k9/src/main/java/pomClass/ClassB.java
@@ -0,0 +1,14 @@
+package pomClass;
+
+public class ClassB {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("git");
+		System.out.println("git");
+		System.out.println("git");
+		System.out.println("git");
+		
+	}
+
+}
---
+++ Kite_k9/src/main/java/pomClass/ClassC.java
@@ -0,0 +1,10 @@
+package pomClass;
+
+public class ClassC {
+
+	public static void main(String[] args) {
+		System.out.println("Hello");
+
+	}
+
+}
