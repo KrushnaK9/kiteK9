577eee34854931d0900c4cc72e9e316caeecbb3a
unknown <kirdat.smita@gmail.com>
2022-12-21 16:16:05 +0530

    This is Test Code by Smita Mane

---
+++ Kite_k9/src/main/java/pomClass/TestClass.java
@@ -0,0 +1,12 @@
+package pomClass;
+
+public class TestClass {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("git");
+		System.out.println("git");
+		
+	}
+
+
+}
